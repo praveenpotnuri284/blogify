504673b799c9c7fb981c7960646433a4a54154c7
praveenpotnuri284 <praveen@workspace.local>
2023-12-26 23:52:28 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const express = require("express");
+
+//server
+
+const app= express();
+
+
+const server = http.createServer(app);
+
+//? start the servert
+
+
+const PORT = process.env.PORT || 9080
+server.listen(PORT,console.log(`server is running good on ${PORT}`));
+
+
